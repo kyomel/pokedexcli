7f0845bb2f04d7db621fc5c2af3441f09f38ed42
kyomel <michaellapandio04@gmail.com>
2024-06-12 22:18:39 +0700

    add cache at pokedex

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -2,17 +2,20 @@ package pokeapi
 
 import (
 	"net/http"
+	"pokedexcli/internal/pokecache"
 	"time"
 )
 
 // Client -
 type Client struct {
+	cache      pokecache.Cache
 	httpClient *http.Client
 }
 
 // NewClient -
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: &http.Client{
 			Timeout: timeout,
 		},
