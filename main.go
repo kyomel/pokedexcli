333783cb151c94f16092f7b1c84f9ccab41d90a1
kyomel <michaellapandio04@gmail.com>
2024-06-17 00:11:07 +0700

    add feature to caught pokemon command

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
 	cfg := &config{
+		caughtPokemon: make(map[string]pokeapi.Pokemon),
 		pokeapiClient: pokeClient,
 	}
 
